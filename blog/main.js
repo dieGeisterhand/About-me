0ab9a20af008aacbcb76e4d312800a4b1e3c7291
j <jeyfrem@proton.me>
2024-05-15 15:03:44 -0400

    done for now

--- blog/main.js
+++ blog/main.js
@@ -1,29 +1,55 @@
-const yearDropdown = document.getElementById("year");
-const monthElement = document.getElementById("month");
-const littleButton = document.getElementById("open-close");
+const yearDropdowns = document.querySelectorAll(".year");
+const monthElements = document.querySelectorAll(".month");
+const littleButtons = document.querySelectorAll(".open-close");
 
-yearDropdown.addEventListener("click", () => {
-  if (monthElement.style.display === "none") {
-    monthElement.style.display = "flex";
-    littleButton.innerHTML = "∧";
-    return;
-  } else {
-    monthElement.style.display = "none";
-    littleButton.innerHTML = "∨"
-  }
-})
+let selectedYear;
 
-const retrieveMonth = (clickedElement) => {
-  const monthOption = clickedElement.innerHTML.toLowerCase();
-  return monthOption;
+for (let i = 0; i < yearDropdowns.length; i++) {
+  yearDropdowns[i].addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    selectedYear = parseInt(clickedElement.textContent);
+    console.log(selectedYear);
+
+    if (monthElements[i].style.display === "none") {
+      monthElements[i].style.display = "flex";
+      littleButtons[i].innerHTML = "∧";
+    } else {
+        monthElements[i].style.display = "none";
+        littleButtons[i].innerHTML = "∨"
+    }
+  });
 }
 
+let monthOption;
 
-const iframeContent = () => {
+for (let i = 0; i < monthElements.length; i++) {
+  monthElements[i].addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    monthOption = clickedElement.innerHTML.toLowerCase();
+    console.log(monthOption + selectedYear);
+
+    updateIframe(monthOption, selectedYear);
+  });
+}
+
+function updateIframe(monthOption, selectedYear) {
   const iframe = document.getElementById("iframe");
-  const month = retrieveMonth(document.querySelector(".month-selection"));
-  
-  if (month === "may") {
+
+  console.log(`Month: ${monthOption}`);
+  console.log(`Year: ${selectedYear}`);
+
+
+  if (monthOption === "july" && selectedYear === 2024) {
+    console.log("Setting iframe src to jeyfrem.com");
     iframe.setAttribute("src", "https://www.jeyfrem.com/");
+  } else if (monthOption === "may") {
+    console.log("Setting iframe src to google.com");
+    iframe.setAttribute("src", "https://www.google.com/");
+  } else if (monthOption === "july" && selectedYear === 2025) {
+    console.log("Setting iframe src to w3schools.com");
+    iframe.setAttribute("src", "https://www.w3schools.com/");
+  } else {
+    console.log("No match. Month Option:", monthOption, "Selected Year:", selectedYear);
+    iframe.setAttribute("src", "");
   }
-};
+}
