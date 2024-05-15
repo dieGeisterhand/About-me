444a494ec19003bd843ce6e32562e77c85d5a818
j <jeyfrem@proton.me>
2024-05-15 10:03:39 -0400

    done for now this time

--- blog/main.js
+++ blog/main.js
@@ -1,11 +1,14 @@
 const year = document.getElementById("year");
 const month = document.getElementById("month");
+const littleButton = document.getElementById("open-close");
 
 year.addEventListener("click", () => {
   if (month.style.display === "none") {
     month.style.display = "flex";
+    littleButton.innerHTML = "∧";
     return;
   } else {
     month.style.display = "none";
+    littleButton.innerHTML = "∨"
   }
 })
